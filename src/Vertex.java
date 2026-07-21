e68e88b1debf0f01f0c8b53671af4f8892dcdd9d
Voon CH <voonch85@gmail.com>
2026-07-21 14:51:46 +0800

    Add a previous field

--- src/Vertex.java
+++ src/Vertex.java
@@ -5,12 +5,14 @@ public class Vertex {
     String name;
     boolean visited;
     int distance;
+    Vertex previous;
     ArrayList<Edge> edges;
 
     public Vertex(String name) {
         this.name = name;
         this.visited = false;
         this.distance = Integer.MAX_VALUE;
+        this.previous = null;
         this.edges = new ArrayList<>();
     }
 
