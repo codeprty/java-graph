a332f7dd8638d52372cee8fd47f5da1e0d48adf4
Voon CH <voonch85@gmail.com>
2026-07-21 14:04:07 +0800

    Add a visited field to the Vertex class

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Vertex {
 
     String name;
+    boolean visited;
     ArrayList<Edge> edges;
 
     public Vertex(String name) {
         this.name = name;
+        this.visited = false;
         this.edges = new ArrayList<>();
     }
 
