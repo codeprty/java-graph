ca757c1635749cfe1768965c26eae74dcad5d4a6
Voon CH <voonch85@gmail.com>
2026-07-21 14:02:36 +0800

    Implement findVertex(), addEdge(), and displayGraph()

--- src/Graph.java
+++ src/Graph.java
@@ -12,4 +12,50 @@ public class Graph {
         vertices.add(new Vertex(name));
     }
 
+    private Vertex findVertex(String name) {
+
+        for (Vertex vertex : vertices) {
+
+            if (vertex.name.equals(name)) {
+                return vertex;
+            }
+
+        }
+
+        return null;
+
+    }
+
+    public void addEdge(String sourceName, String destinationName, int weight) {
+
+        Vertex source = findVertex(sourceName);
+        Vertex destination = findVertex(destinationName);
+
+        if (source == null || destination == null) {
+            System.out.println("Vertex not found.");
+            return;
+        }
+
+        source.edges.add(new Edge(destination, weight));
+        destination.edges.add(new Edge(source,weight));
+
+    }
+
+    public void displayGraph() {
+
+        for (Vertex vertex : vertices) {
+
+            System.out.print(vertex.name + " -> ");
+
+            for (Edge edge : vertex.edges) {
+                System.out.print(edge.destination.name
+                        + "(" + edge.weight + ") ");
+            }
+
+        }
+
+        System.out.println();
+
+    }
+
 }
