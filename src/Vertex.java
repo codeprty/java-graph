78959fc7699cda97d10971d2b02d6b082f1b215b
Voon CH <voonch85@gmail.com>
2026-07-21 14:46:03 +0800

    Add a distance field

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,11 +4,13 @@ public class Vertex {
 
     String name;
     boolean visited;
+    int distance;
     ArrayList<Edge> edges;
 
     public Vertex(String name) {
         this.name = name;
         this.visited = false;
+        this.distance = Integer.MAX_VALUE;
         this.edges = new ArrayList<>();
     }
 
