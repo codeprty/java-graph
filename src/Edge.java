5fe05da1e3dd2619a2eb39681c70cc814ca328d3
Voon CH <voonch85@gmail.com>
2026-07-21 16:40:58 +0800

    Add source vertex

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,11 @@
 public class Edge {
 
+    Vertex source;
     Vertex destination;
     int weight;
 
-    public Edge(Vertex destination, int weight) {
+    public Edge(Vertex source, Vertex destination, int weight) {
+        this.source = source;
         this.destination = destination;
         this.weight = weight;
     }
